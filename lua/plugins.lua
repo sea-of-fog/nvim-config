bf8c94da4701582fa2c2c4dbc7441ea173697f92
Krzysztof Szymański <kszymanski926@gmail.com>
2023-04-07 18:23:39 +0200

    First commit

---
+++ lua/plugins.lua
@@ -0,0 +1,11 @@
+
+return require('packer').startup(function(use)
+
+	use 'wbthomason/packer.nvim'
+	use "jiangmiao/auto-pairs"
+	use "vimwiki/vimwiki"
+	use "SirVer/ultisnips"
+
+
+
+end)
---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+local set = vim.opt
+
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.clipboard = "unnamedplus"
